0886132d23c72c6227476b59528a2ca8a2051522
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-29 19:40:02 +0200

    add contact section html css

--- index.php
+++ index.php
@@ -506,6 +506,25 @@
     </section>
     <!-- Review section End-->
 
+    <!--Contact form Start-->
+    <section id="Contact" class="contact">
+        <h1 class="heading">Nous Contacter</h1>
+
+        <div class="row">
+            <iframe class="iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.950066895727!2d1.4107371762735332!3d43.56592355807125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aebbcf48268f93%3A0x3a982bed4911e80e!2s226%20Rte%20de%20Seysses%2C%2031100%20Toulouse!5e0!3m2!1sfr!2sfr!4v1687799117781!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+            <form action="">
+                <h3>Rendez-Vous</h3>
+                <input type="text" placeholder="Votre Nom" required class="box">
+                <input type="text" placeholder="Votre Prénom" required class="box">
+                <input type="email" placeholder="Votre Email" required class="box">
+                <textarea class="box" required placeholder="Votre Message" id="" cols="30" rows="10"></textarea>
+                <button class="btn">Envoyer</button>
+            </form>
+        </div>
+    </section>
+    <!--Contact form End-->
+
 
     <!-- link JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
