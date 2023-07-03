bf9b56d20679391129ded1a13da121723f2ab0b1
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-03 21:23:35 +0200

    add pictures.php

---
+++ Templates/addImg.php
@@ -0,0 +1,73 @@
+<style>
+    * {
+        font-size: 1.5rem;
+    }
+
+    .h1 {
+        text-shadow: 10px 10px 10px red;
+    }
+
+    #Pictures {
+        margin: 10rem 0;
+    }
+
+    .bg {
+        background: var(--main);
+        padding: 2rem;
+        box-shadow: 0 0 15px black;
+        border-radius: 1rem;
+
+
+    }
+
+    .border {
+        border: 2px solid #fff;
+    }
+</style>
+
+
+<main class="container-fluid" id="Pictures">
+    <div class="row h1">
+        <div class="col-12 text-center p-5 fw-bold">
+            <h1>LES PHOTOS</h1>
+        </div>
+    </div>
+    <div class="container bg col-md-5 col-md-offset-3 ">
+        <div class="panel panel-info ">
+            <div class="panel-heading text-center  text-white fs-1 py-5">
+                Formulaire Ajout une Image
+            </div>
+            <div class="panel-body ">
+                <div class="row">
+                    <div class="col-md-12"><?php echo $valid; ?></div>
+                </div>
+                <form method="post" action="" enctype="multipart/form-data">
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label text-white ">Nom</label><br>
+                        <div class="col-sm-12">
+                            <input type="text" name="textnom" class="form-control p-3">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label text-white ">Info</label><br>
+                        <div class="col-sm-12">
+                            <input type="text" name="textinfo" class="form-control p-3">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label text-white ">Photo</label><br>
+                        <div class="col-sm-12">
+                            <input type="file" name="img" class="form-control-file p-3">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label"></label>
+                        <div class="col-sm-12">
+                            <input type="submit" name="btnAjout" class="btn btn-primary border rounded-0 p-2">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
---
+++ config/upload_image.php
@@ -0,0 +1,37 @@
+<?php
+require_once "config/db.php";
+
+$photo = "";
+$valid = "";
+//Bouton Ajouter :
+if (isset($_POST['btnAjout'])) {
+    //echo "Ok";
+
+    // variable 
+    $nom = $_POST['txtnom'];
+    $info = $_POST['txtinfo'];
+
+    // Ajout image :
+    $photo = $_FILES['img']['name'];
+    $upload = "IMG-FILE/" . $photo;
+
+    move_uploaded_file($_FILES['img']['tmp_name'], $upload);
+
+    //requete ajout query exercute
+    $sql = "INSERT INTO tb_image(nom,info,photo) VALUES('$nom', '$info' , '$photo')";
+    if (mysqli_query($conn, $sql)) {
+        // echo "image Ajoutée avec succé.";
+        $valid = "<div class='alert alert-success'>
+                    <a href='#' class='close'
+                    data-dismiss='alert'
+                    aria-label='close'>&times;</a><b>Image Ajout avec success.</b>
+                </div>";
+    } else {
+        //echo "Erreur ajout image !";
+        $valid = "<div class='alert alert-dabger'>
+                    <a href='#' class='close'
+                    data-dismiss='alert'
+                    aria-label='close'>&times;</a><b>Erreur ajout image !</b>
+                </div>";
+    }
+}
