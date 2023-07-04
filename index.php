00ec72d153e9ec6178017cff6d36eed5f970e312
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-05 00:03:22 +0200

    add all

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once __DIR__ . "/config/config.php";
 require_once __DIR__ . "/Templates/header.php";
 require_once __DIR__ . "/Templates/login.php";
 require_once __DIR__ . "/Templates/home.php";
