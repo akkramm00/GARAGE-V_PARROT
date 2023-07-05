32644a2b0207d47921e89615674fa009177c0cdf
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-05 23:40:07 +0200

    add all modification

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,8 +1,59 @@
 <!-- Section Footer Start-->
-<footer>
-    <a href="mailto:G.parrot@yahoo.fr"><i class="fas  fa-envelope"></i>G.parrot@yahoo.fr</a>
-    <a href="#">&copy; Tous Droits réservés</a>
-    <a href="tel:(+33) 123456789"><i class="fas  fa-phone"></i></i>(+33) 123456789</a>
+<footer class="container-fluid ">
+    <div class="row d-flex justify-content-center align-items-center">
+        <div class="col-12 py-5">
+            <table class="table table-dark table-striped table-hover box-ch my-3 py-5 text-white fs-4 bg-dark ">
+                <thead>
+                    <tr>
+                        <th scope="col">Jours</th>
+                        <th scope="col">Matin</th>
+                        <th scope="col">Après-midi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Lundi</td>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Mardi</td>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Mercredi</th>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Jeudi</th>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Vendredi</th>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Samedi</th>
+                        <td>8:00 - 12:00</td>
+                        <td>13:30 - 18:00</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Dimanche</th>
+                        <td colspan="2" style="text-align: center;">fermé</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-12 py-3 footerA">
+            <a href="mailto:G.parrot@yahoo.fr"><i class="fas  fa-envelope"></i>G.parrot@yahoo.fr</a>
+            <a href="#">&copy; Tous Droits réservés</a>
+            <a href="tel:(+33) 123456789"><i class="fas  fa-phone"></i></i>(+33) 123456789</a>
+        </div>
+    </div>
 </footer>
 <!-- Section Footer End-->
 
--- Templates/vehicules.php
+++ Templates/vehicules.php
@@ -1,91 +1,143 @@
-<!-- Vehicules Section Start -->
-<section id="Vehicule" class="vehicule">
-    <h1 class="heading">Voitures a Vendre</h1>
-    <div class="swiper VehiculesSlider">
-        <div class="swiper-wrapper">
+ <!--Vehicules Section Start -->
 
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 1.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 2.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 3.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 4.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 5.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-            <div class="swiper-slide box">
-                <img src="IMG-FILE\MAZDA 3.webp" alt="">
-                <div class="content">
-                    <h3>New Model</h3>
-                    <div class="price"><span>Price</span>$102.000</div>
-                    <p>
-                        <span class="fas fa-circle"></span>2022
-                        <span class="fas fa-circle"></span>Automatic
-                        <span class="fas fa-circle"></span>Electric
-                        <span class="fas fa-circle"></span>200mph
-                    </p>
-                </div>
-            </div>
-        </div>
+ <section id="Vehicule" class="vehicule">
+     <h1 class="heading">Voitures a Vendre</h1>
+     <div class="swiper VehiculesSlider">
+         <div class="swiper-wrapper">
 
-        <div class="swiper-pagination"></div>
+             <div class="swiper-slide box">
+                 <?php
+                    // Connexion à la base de données
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $dbname = "php_image";
 
-    </div>
-</section>
-<!-- Vehicules Section end -->
+                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    if ($conn->connect_error) {
+                        die("Erreur de connexion à la base de données : " . $conn->connect_error);
+                    }
+
+                    // Récupération des données depuis la base de données
+                    $sql = "SELECT * FROM tb_voitures";
+                    $result = $conn->query($sql);
+
+                    // Vérification s'il y a des données à afficher
+                    if ($result->num_rows > 0) {
+                        foreach ($result as $row) {
+                    ?>
+                         <img src="<?php echo $row['Image']; ?>." alt="image">
+                         <div class="content">
+                             <h3><?php echo $row['Model']; ?></h3>
+                             <div class="price"><span>Prix</span><?php echo $row['Prix']; ?></div>
+                             <p>
+                                 <span class="fas fa-circle"></span><?php echo $row['Année']; ?>
+                                 <span class="fas fa-circle"></span><?php echo $row['Boite']; ?>
+                                 <span class="fas fa-circle"></span><?php echo $row['Energie']; ?>
+                                 <span class="fas fa-circle"></span><?php echo $row['Puissance']; ?>
+                             </p>
+                         </div>
+                 <?php
+                        }
+                    } else {
+                        echo "Aucune donnée à afficher.";
+                    }
+
+                    // Fermeture de la connexion à la base de données
+                    $conn->close();
+                    ?>
+             </div>
+         </div>
+         <div class="swiper-pagination"></div>
+     </div>
+ </section>
+ <!--Vehicules Section Start -->
+
+ <section id="Vehicule" class="vehicule">
+     <h1 class="heading">Voitures a Vendre</h1>
+     <div class="swiper VehiculesSlider">
+         <div class="swiper-wrapper">
+
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 1.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 2.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 3.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 4.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 5.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-slide box">
+                 <img src="IMG-FILE\MAZDA 3.webp" alt="">
+                 <div class="content">
+                     <h3>New Model</h3>
+                     <div class="price"><span>Price</span>$102.000</div>
+                     <p>
+                         <span class="fas fa-circle"></span>2022
+                         <span class="fas fa-circle"></span>Automatic
+                         <span class="fas fa-circle"></span>Electric
+                         <span class="fas fa-circle"></span>200mph
+                     </p>
+                 </div>
+             </div>
+             <div class="swiper-pagination"></div>
+         </div>
+ </section>
+
+ <!-- Vehicules Section end -->
