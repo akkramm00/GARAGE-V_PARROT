b124bb309ce256118930a09ad115b95b5b2fa26b
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-10 23:51:43 +0200

    add all

--- dates.php
+++ dates.php
@@ -1,10 +1,44 @@
 <?php
 require_once __DIR__ . "/config/config.php";
 require_once __DIR__ . "/Templates/header.php";
+
+// Fonction pour supprimer un rendez-vous
+function supprimerRendezVous($id)
+{
+    // Connexion à la base de données
+    $conn = new mysqli('localhost', 'root', '', 'users');
+
+    // Vérifier la connexion à la base de données
+    if ($conn->connect_error) {
+        die("La connexion à la base de données a échoué : " . $conn->connect_error);
+    }
+
+    // Préparer la requête de suppression
+    $stmt = $conn->prepare("DELETE FROM rendez_vous WHERE id = ?");
+    $stmt->bind_param("i", $id);
+
+    // Exécuter la requête de suppression
+    if ($stmt->execute()) {
+        echo "Rendez-vous supprimé avec succès.";
+    } else {
+        echo "Une erreur s'est produite lors de la suppression du rendez-vous : " . $stmt->error;
+    }
+
+    // Fermeture de la connexion à la base de données
+    $stmt->close();
+    $conn->close();
+}
+
 ?>
+
 <style>
     body {
         margin-top: 15rem;
+    }
+
+    .txt-w {
+        text-decoration: none;
+        color: white;
     }
 </style>
 
@@ -22,17 +56,12 @@ require_once __DIR__ . "/Templates/header.php";
                         <th>Email</th>
                         <th>Services</th>
                         <th>Message</th>
-                        <th>Actions</th> <!-- Nouvelle colonne pour les boutons -->
+                        <th>Actions</th>
                     </tr>
 
                     <?php
                     // Connexion à la base de données
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $dbname = "users";
-
-                    $conn = new mysqli($servername, $username, $password, $dbname);
+                    $conn = new mysqli('localhost', 'root', '', 'users');
 
                     // Vérifier la connexion à la base de données
                     if ($conn->connect_error) {
@@ -40,7 +69,7 @@ require_once __DIR__ . "/Templates/header.php";
                     }
 
                     // Récupérer les données de la table rendez_vous
-                    $sql = "SELECT nom, prenom, email, services, message FROM rendez_vous";
+                    $sql = "SELECT id, nom, prenom, email, services, message FROM rendez_vous";
                     $result = $conn->query($sql);
 
                     // Afficher les données dans le tableau
@@ -53,8 +82,11 @@ require_once __DIR__ . "/Templates/header.php";
                             echo "<td>" . $row["services"] . "</td>";
                             echo "<td>" . $row["message"] . "</td>";
                             echo "<td>";
-                            echo "<button class='btn btn-primary' name='Ajouter'><a href='../#Contact'>Ajouter</a></button>";
-                            echo "<button class='btn btn-danger' name='Supprimer'>Supprimer</button>";
+                            echo "<form method='post' action=''>";
+                            echo "<input type='hidden' name='id' value='" . $row["id"] . "'>";
+                            echo "<button class='btn btn-primary t-decoration-none txt-w mx-1 p-0 fs-0.5rem' type='submit' name='ajouter'><a href='../#Contact'>Ajouter</a></button>";
+                            echo "<button class='btn btn-danger  mx-1 p-0 fs-0.5rem text-white text-decoration-none' type='submit' name='supprimer'>Supprimer</button>";
+                            echo "</form>";
                             echo "</td>";
                             echo "</tr>";
                         }
@@ -65,12 +97,20 @@ require_once __DIR__ . "/Templates/header.php";
                     // Fermeture de la connexion à la base de données
                     $conn->close();
                     ?>
+
                 </table>
             </div>
         </div>
     </div>
 
 </body>
+
 <?php
 require_once "Templates/footer.php";
+
+// Traitement de la soumission du formulaire de suppression
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['supprimer'])) {
+    $id = $_POST['id'];
+    supprimerRendezVous($id);
+}
 ?>
